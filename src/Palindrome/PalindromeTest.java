26066f59768afedb4e94a9c24998adcbcdbd043f
Your Name <you@example.com>
2024-12-18 22:48:41 +0700

    edit test for factorial and palindrome

--- src/Palindrome/PalindromeTest.java
+++ src/Palindrome/PalindromeTest.java
@@ -3,92 +3,64 @@ package Palindrome;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeTest {
 
-    public static Palindrome open = new Palindrome();
+    /**
+     * Helper method to compare the results of all palindrome methods.
+     */
+    private void checkRecursivePalindromeCorrectness(String input) {
+        LinkedList<Character> list = Palindrome.stringToLinkedList(input);
 
-    @Test
-    void try_fail()
-    {
-//        assertEquals();
-        fail();
+        boolean correctResult = Palindrome.palindromeCorrect(new LinkedList<>(list));
+        boolean recursiveResult = Palindrome.palindromeWTHRecursive(new LinkedList<>(list));
+
+        assertEquals(correctResult, recursiveResult);
     }
 
     @Test
-    @DisplayName("This for test correct version of palindrome")
-    void test1()
-    {
-
-        assertAll
-                (
-                        () -> assertEquals(true, open.palindrome_correct(open.string_to_linkedlist("Mom"))),
-                        () -> assertEquals(true, open.palindrome_correct(open.string_to_linkedlist("Dad"))),
-                        () -> assertEquals(true, open.palindrome_correct(open.string_to_linkedlist("POP"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("Mono Poly"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("ManekiNeko"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("Blue Archive"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("KaliYuunai"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("kaliYuunii"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("Polar bear"))),
-                        () -> assertEquals(false, open.palindrome_correct(open.string_to_linkedlist("OREO")))
-                );
+    @DisplayName("Test Palindromes")
+    void testPalindromes() {
+        checkRecursivePalindromeCorrectness("Mom");
+        checkRecursivePalindromeCorrectness("abba");
+        checkRecursivePalindromeCorrectness("PoP");
+        checkRecursivePalindromeCorrectness("RaceCar");
+        checkRecursivePalindromeCorrectness("A");
     }
 
     @Test
-    @DisplayName("Test 3 character")
-    void test2()
-    {
-        boolean correct1 = open.palindrome_correct(open.string_to_linkedlist("Mom"));
-        boolean correct2 = open.palindrome_correct(open.string_to_linkedlist("dad"));
-        boolean correct3 = open.palindrome_correct(open.string_to_linkedlist("PoP"));
-
-        assertEquals(correct1, open.palindrome_WTH_REC(open.string_to_linkedlist("Mom")));
-        assertEquals(correct2, open.palindrome_WTH_REC(open.string_to_linkedlist("dad")));
-        assertEquals(correct3, open.palindrome_WTH_REC(open.string_to_linkedlist("PoP")));
-
-        assertEquals(correct1, open.palindrome_WTH_LOO(open.string_to_linkedlist("Mom")));
-        assertEquals(correct2, open.palindrome_WTH_LOO(open.string_to_linkedlist("dad")));
-        assertEquals(correct3, open.palindrome_WTH_LOO(open.string_to_linkedlist("PoP")));
+    @DisplayName("Test Non-Palindromes")
+    void testNonPalindromes() {
+        checkRecursivePalindromeCorrectness("Mono Poly");
+        checkRecursivePalindromeCorrectness("ManekiNeko");
+        checkRecursivePalindromeCorrectness("Blue Archive");
+        checkRecursivePalindromeCorrectness("KaliYuunai");
+        checkRecursivePalindromeCorrectness("Polar bear");
     }
 
     @Test
-    void test_not_palindrome()
-    {
-        boolean correct1 = open.palindrome_correct(open.string_to_linkedlist("Mono Poly"));
-        boolean correct2 = open.palindrome_correct(open.string_to_linkedlist("ManekiNeko"));
-        boolean correct3 = open.palindrome_correct(open.string_to_linkedlist("Blue Archive"));
-        boolean correct4 = open.palindrome_correct(open.string_to_linkedlist("KaliYuunai"));
-        boolean correct5 = open.palindrome_correct(open.string_to_linkedlist("kaliYuunii"));
-        boolean correct6 = open.palindrome_correct(open.string_to_linkedlist("Polar bear"));
+    @DisplayName("Test Edge Cases")
+    void testEdgeCases() {
+        // Empty string
+        checkRecursivePalindromeCorrectness("");
 
-        assertEquals(correct1, open.palindrome_WTH_REC(open.string_to_linkedlist("Mono Poly")));
-        assertEquals(correct1, open.palindrome_WTH_LOO(open.string_to_linkedlist("Mono Poly")));
+        // Single character
+        checkRecursivePalindromeCorrectness("A");
+        checkRecursivePalindromeCorrectness("z");
 
-        assertEquals(correct2, open.palindrome_WTH_REC(open.string_to_linkedlist("ManekiNeko")));
-        assertEquals(correct2, open.palindrome_WTH_LOO(open.string_to_linkedlist("ManekiNeko")));
-
-        assertEquals(correct3, open.palindrome_WTH_REC(open.string_to_linkedlist("Blue Archive")));
-        assertEquals(correct3, open.palindrome_WTH_LOO(open.string_to_linkedlist("Blue Archive")));
-
-        assertEquals(correct4, open.palindrome_WTH_REC(open.string_to_linkedlist("KaliYuunai")));
-        assertEquals(correct4, open.palindrome_WTH_LOO(open.string_to_linkedlist("KaliYuunai")));
-
-        assertEquals(correct5, open.palindrome_WTH_REC(open.string_to_linkedlist("kaliYuunii")));
-        assertEquals(correct5, open.palindrome_WTH_LOO(open.string_to_linkedlist("kaliYuunii")));
-
-        assertEquals(correct6, open.palindrome_WTH_REC(open.string_to_linkedlist("Polar bear")));
-        assertEquals(correct6, open.palindrome_WTH_LOO(open.string_to_linkedlist("Polar bear")));
+        // Case sensitivity
+        checkRecursivePalindromeCorrectness("oREo");
+        checkRecursivePalindromeCorrectness("AbcBA");
     }
 
     @Test
-    @DisplayName("Test oReo case")
-    void test()
-    {
-        boolean correct6 = open.palindrome_correct(open.string_to_linkedlist("oREo"));
-
-        assertEquals(correct6, open.palindrome_WTH_REC(open.string_to_linkedlist("oREo")));
-        assertEquals(correct6, open.palindrome_WTH_LOO(open.string_to_linkedlist("oREo")));
+    @DisplayName("Test Exception for Null Input")
+    void testNullInput() {
+        assertThrows(NullPointerException.class, () -> Palindrome.palindromeCorrect(null));
+        assertThrows(NullPointerException.class, () -> Palindrome.palindromeWTHRecursive(null));
+        assertThrows(NullPointerException.class, () -> Palindrome.palindromeWTHLoop(null));
     }
 }
